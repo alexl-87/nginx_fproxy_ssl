59902a3694360ed8b3deb5ffb07709d52e2075fd
alatyshe <alexander.latyshev@harmonicinc.com>
2020-10-19 13:36:44 +0300

    Mon, Oct 19, 2020  1:36:44 PM

---
+++ nginx_api.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var app = express();
+var exec = require("child_process").exec;
+app.get('/api/docker/run/:port', function (req, res) {
+    var command = "docker run -t -d -p " + req.params.port + ":443 --name NGINX-" + req.params.port + " --hostname NGINX-" + req.params.port + " nginx_forward_proxy";
+    runCommand(command);
+    res.send("SUCCESS");
+});
+app.get('/api/docker/rm/:port', function (req, res) {
+    var command = "docker rm -f NGINX-" + req.params.port;
+    runCommand(command);
+    res.send("SUCCESS");
+});
+app.get('/api/docker/:port/:response', function (req, res) {
+    var command = "docker exec -its NGINX-" + req.params.port + " /usr/local/nginx/sbin/nginx_ " + req.params.response + ".sh";
+    runCommand(command);
+    res.send("SUCCESS");
+});
+function runCommand(cmd, callback) {
+    if (callback == undefined) {
+        return new Promise(function (resolve, reject) {
+            exec(cmd, function (error, stdout, stderr) {
+                if (error != undefined) {
+                    reject(error);
+                }
+                if (stderr) {
+                    console.log("" + stderr);
+                }
+                resolve(stdout);
+            });
+        });
+    }
+}
+app.listen(8880, function () { return console.log('listening on port 8880...'); });
