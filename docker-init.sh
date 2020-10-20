2374bf04270dc69756a731034e63e525cdafdf10
alatyshe <alexander.latyshev@harmonicinc.com>
2020-10-20 10:53:42 +0300

    Tue, Oct 20, 2020 10:53:42 AM

--- docker-init.sh
+++ docker-init.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 docker build -t nginx_forward_proxy:latest /home/harmonic/nginx
+chmod 0755 /home/harmonic/nginx/nginx_api.js
 mv /home/harmonic/nginx/nginx_api.js /home/harmonic/nodejs/nginx_api.js 
-mv /home/harmonic/nginx/nginx_api.service /etc/systemd/system/nginx_api.service
+mv /home/harmonic/nginx/nginx_api.service /lib/systemd/system/nginx_api.service
 systemctl start nginx_api
 
