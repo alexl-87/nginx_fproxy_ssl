a51cab4d54208e6be2fe4ca6183c7dc8fcae435e
alatyshe <alexander.latyshev@harmonicinc.com>
2020-11-02 17:24:56 +0200

    Mon, Nov  2, 2020  5:24:56 PM

--- nginx_200.sh
+++ nginx_200.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-cp /usr/local/nginx/conf/nginx_conf_200 /usr/local/nginx/conf/nginx.conf
+cp /usr/local/nginx/conf/nginx_conf_allow /usr/local/nginx/conf/nginx.conf
 /usr/local/nginx/sbin/nginx -s reload
