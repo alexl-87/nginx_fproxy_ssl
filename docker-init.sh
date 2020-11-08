d9a6cf785a60f076023ee1f28488f45210430e13
alatyshe <alexander.latyshev@harmonicinc.com>
2020-11-08 18:16:51 +0200

    Sun, Nov  8, 2020  6:16:51 PM

--- docker-init.sh
+++ docker-init.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-docker build -t nginx_forward_proxy:latest /home/nginx/nginx > docker_init.log
+docker build -t nginx_forward_proxy:latest /home/nginx/nginx
 cp /home/nginx/nginx/nginx_api.ts /home/nginx/nodejs
 chmod 0755 /home/nginx/nginx/nginx_api.ts
 cp /home/nginx/nginx/nginx_api.service /lib/systemd/system
