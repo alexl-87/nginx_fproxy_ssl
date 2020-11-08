123b0055677231859fbef9029e376d49fc19da8a
alatyshe <alexander.latyshev@harmonicinc.com>
2020-11-08 18:32:20 +0200

    Sun, Nov  8, 2020  6:32:20 PM

--- docker-init.sh
+++ docker-init.sh
@@ -1,8 +1,13 @@
 #! /bin/bash
+
+# Build nginx docker image
 docker build -t nginx_forward_proxy:latest /home/nginx/nginx
+
+# Compile VM's api
 tsc /home/nginx/nginx/nginx_api.ts
+
+# Copy files and run service
 cp /home/nginx/nginx/nginx_api.js /home/nginx/nodejs
-chmod 0755 /home/nginx/nginx/nginx_api.js
 cp /home/nginx/nginx/nginx_api.service /lib/systemd/system
 systemctl start nginx_api
 
